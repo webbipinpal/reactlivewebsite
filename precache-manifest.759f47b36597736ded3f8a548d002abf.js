c2904f301130070a2cee50604ba813cd450202b0
Bipin Kumar Pal <bpal@hbku.edu.qa>
2023-02-09 14:16:21 +0300

    Updates

---
+++ precache-manifest.759f47b36597736ded3f8a548d002abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b75e99b17e13b1530598",
+    "url": "/reactlivewebsite/static/css/main.70aa3ce1.chunk.css"
+  },
+  {
+    "revision": "b75e99b17e13b1530598",
+    "url": "/reactlivewebsite/static/js/main.b75e99b1.chunk.js"
+  },
+  {
+    "revision": "575592be2d3e253bb90c",
+    "url": "/reactlivewebsite/static/js/1.575592be.chunk.js"
+  },
+  {
+    "revision": "7cb3dfef5fe1e343d8f2",
+    "url": "/reactlivewebsite/static/css/2.2b0402c2.chunk.css"
+  },
+  {
+    "revision": "7cb3dfef5fe1e343d8f2",
+    "url": "/reactlivewebsite/static/js/2.7cb3dfef.chunk.js"
+  },
+  {
+    "revision": "34782f7e04eae5eee21f",
+    "url": "/reactlivewebsite/static/js/runtime~main.34782f7e.js"
+  },
+  {
+    "revision": "378e461504adbd75e4d89db9ac80b136",
+    "url": "/reactlivewebsite/index.html"
+  }
+];
